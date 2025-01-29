5b6fb5bcb5a1cd64809b8f925187546565640b87
KevinDonovan2 <hei.donovan.2@gmail.com>
2025-01-29 17:35:49 +0300

    feat: config dataprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,96 @@
-import { Admin, Resource, ListGuesser } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import {
+  Admin,
+  Create,
+  Datagrid,
+  DeleteButton,
+  Edit,
+  EditButton,
+  List,
+  ListGuesser,
+  required,
+  Resource,
+  Show,
+  SimpleForm,
+  SimpleShowLayout,
+  TextField,
+  TextInput,
+  TopToolbar,
+} from 'react-admin';
+import { v4 as uuid } from 'uuid';
+import { raDataProvider } from './providers';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-
-function App() {
+export const CreatePerson = () => {
   return (
-    <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={ListGuesser} />
+    <Create
+      transform={(data) => ({ ...data, id: uuid() })}
+      mutationOptions={{
+        meta: {
+          mutationType: 'CREATE',
+        },
+      }}
+    >
+      <SimpleForm>
+        <TextInput validate={required()} source="name" label="Name" />
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export const EditPerson = () => {
+  return (
+    <Edit
+      mutationOptions={{
+        meta: {
+          mutationType: 'EDIT',
+        },
+      }}
+    >
+      <SimpleForm>
+        <TextInput validate={required()} source="name" label="Name" />
+      </SimpleForm>
+    </Edit>
+  );
+};
+
+export const PersonList = () => (
+  <List empty={false}>
+    <Datagrid>
+      <TextField source="id" />
+      <TextField source="name" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+export const PersonShow = () => (
+  <Show
+    actions={
+      <TopToolbar>
+        <DeleteButton />
+      </TopToolbar>
+    }
+  >
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="name" />
+    </SimpleShowLayout>
+  </Show>
+);
+
+export const App = () => {
+  return (
+    <Admin
+      title="ra-data-provider-wrapper-examples"
+      dataProvider={raDataProvider}
+    >
+      <Resource
+        name="persons"
+        list={<PersonList />}
+        create={<CreatePerson />}
+        edit={<EditPerson />}
+        show={<PersonShow />}
+      />
+      <Resource name="error" list={<ListGuesser />} />
     </Admin>
   );
-}
-
-export default App;
+};
